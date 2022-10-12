0e4c77fd5e733d4573bed44ce89f8e71aa19a7d5
Luis <luiveldel@alum.us.es>
2022-10-12 14:40:15 +0200

    upload exercise 7_1

---
+++ Ejercicio7_1/main.py
@@ -0,0 +1,12 @@
+import calculadora
+
+
+def main():
+    print(calculadora.suma(2, 2))
+    print(calculadora.resta(5, 2))
+    print(calculadora.multiplica(3, 3))
+    print(calculadora.divide(5, 2))
+
+
+if '__main__' == __name__:
+    main()
