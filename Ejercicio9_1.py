fe4f4e1fe9988a2632a59469c3411a814dcd48ad
Luis <luiveldel@alum.us.es>
2022-10-13 01:29:11 +0200

    upload exercise 9_1

---
+++ Ejercicio9_1.py
@@ -0,0 +1,10 @@
+"""
+Crea un script que le pida al usuario una lista de países (separados por comas). Éstos se deben almacenar en una lista.
+No debería haber países repetidos (haz uso de set).
+Finalmente, muestra por consola la lista de países ordenados alfabéticamente y separados por comas.
+"""
+
+
+countries = set(input('Introduce una lista de paises (separados por coma): ').split(','))
+
+print(sorted(countries))
